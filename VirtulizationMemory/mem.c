13adb153da66179e1901c86c4a650ef702862675
jinesh90 <jinesh90@gmail.com>
2019-05-14 20:06:38 -0700

    adding mem file

---
+++ VirtulizationMemory/mem.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+int main(int argc, char *argv[]){
+    // allocate memory
+    int *p = malloc(sizeof(int));
+    assert(p != NULL);
+
+}
