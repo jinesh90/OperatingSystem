afd617a1ef3ec2f116b94e29556c3d3acff3d1ff
jinesh90 <jinesh90@gmail.com>
2019-05-14 23:25:30 -0700

    adding mem file

--- VirtulizationMemory/mem.c
+++ VirtulizationMemory/mem.c
@@ -1,3 +1,8 @@
+/*
+ * This program will demo how OS is doing memory virtulization,
+ * when you run this program on two different terminals,it will point to the same memory location.
+ * OS is creating virtual address space or just address space for two different programs.
+ */
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +11,16 @@
 int main(int argc, char *argv[]){
     // allocate memory
     int *p = malloc(sizeof(int));
+
     assert(p != NULL);
+
+    printf("(%d) address pointed by p: %p\n",getpid(),p);
+    *p = 0;
+    while(1){
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d) p: %d\n", getpid(), *p);
+    }
+    return 0;
 
 }
